50b08daee66ccadaa54ad910c1178295af654bba
Parimalisgreat <parimal.jichkar104@gmail.com>
2024-04-14 13:09:22 +0530

    added desc to 85. leetcode

--- 85. Maximal Rectangle.cpp
+++ 85. Maximal Rectangle.cpp
@@ -5,3 +5,11 @@
 
 // Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
 
+// Based on Largest Area of Histogram
+// Monotonic Stack
+
+// Expected Complexity =>
+
+// Time: O(n*m)
+
+// Space: O(m)  
